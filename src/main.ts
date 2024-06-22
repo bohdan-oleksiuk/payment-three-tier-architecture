2578ade802c92b239b03ce6c129bc81b285031bd
Bohdan Oleksiuk <bohdan.oleksiuk@ecdevstudio.com>
2024-06-22 11:23:15 +0300

    basic settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Api')
+    .setDescription('docs')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+  const port = configService.get('API_HTTP_PORT') ?? 5000;
+  app.enableCors();
+  await app.listen(+port, () =>
+    console.log(`🚀 Application is running on: http://localhost:${port}/`),
+  );
 }
 bootstrap();
